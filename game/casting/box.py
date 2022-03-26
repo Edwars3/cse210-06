3009adf2b9f0fb28ba572de6d3a6fa5a986dec1c
Angelica <angelicascarante@hotmail.com>
2022-03-26 11:06:40 -0600

    Dot and lines color
    I changed the colors for orange, blue and yellow

--- game/casting/box.py
+++ game/casting/box.py
@@ -2,16 +2,16 @@ from tkinter import *
 import numpy as np
 
 
-#
+
 size_of_board = 600
 number_of_dots = 6
 symbol_size = (size_of_board / 3 - size_of_board / 8) / 2
 symbol_thickness = 50
-dot_color = '#7BC043'
-player1_color = '#0492CF'
-player1_color_light = '#67B0CF'
-player2_color = '#EE4035'
-player2_color_light = '#EE7E77'
+dot_color = '#FFFF00'
+player1_color = '#FE420F'
+player1_color_light = '#FFA500'
+player2_color = '#0000FF'
+player2_color_light = '#069AF3'
 Green_color = '#7BC043'
 dot_width = 0.25*size_of_board/number_of_dots
 edge_width = 0.1*size_of_board/number_of_dots
